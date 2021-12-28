80b6aa1191ea68651dd8b435b899c809adf0f52d
Allan Ramos <allanramos.teixeira@gmail.com>
2021-12-27 23:32:46 -0300

    first commit

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+/Users/allan/dev/hello-rust/hello_cargo/target/debug/deps/hello_cargo: src/main.rs
+
+/Users/allan/dev/hello-rust/hello_cargo/target/debug/deps/hello_cargo.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/allan/dev/hello-rust/hello_cargo/target/debug/hello_cargo: /Users/allan/dev/hello-rust/hello_cargo/src/main.rs
