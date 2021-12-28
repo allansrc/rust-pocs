6dc4aea4b59fa1481c63f87e9f72aaa533265c0e
Allan Ramos <allanramos.teixeira@gmail.com>
2021-12-27 23:36:01 -0300

    update

--- hello_cargo/target/debug/deps/hello_cargo.d
+++
@@ -1,5 +0,0 @@
-/Users/allan/dev/hello-rust/hello_cargo/target/debug/deps/hello_cargo: src/main.rs
-
-/Users/allan/dev/hello-rust/hello_cargo/target/debug/deps/hello_cargo.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/allan/dev/hello-rust/hello_cargo/target/debug/hello_cargo: /Users/allan/dev/hello-rust/hello_cargo/src/main.rs
